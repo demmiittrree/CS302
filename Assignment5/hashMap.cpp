923368f90a815f03269c3c41846b55573116ee85
Demitre Lester <demiitre@BLD.localdomain>
2026-04-13 20:53:32 -0700

    finished insertion

--- Assignment5/hashMap.cpp
+++ Assignment5/hashMap.cpp
@@ -29,11 +29,11 @@ hashMap<t1, t2>::~hashMap()
 {
     // deallocate nodes in table1
     for (std::size_t i = 0 ; i < capacity ; ++i) {
-        delete table1[i]
+        delete table1[i];
     }
     // deallocate nodes in table2
     for (std::size_t i = 0 ; i < capacity ; ++i) {
-        delete table2[i]
+        delete table2[i];
     }
     
     // deallocate table 1 and table 2
@@ -66,9 +66,12 @@ size_t hashMap<t1, t2>::hash2(string k)
     for (int i = 0 ; i < k.length() ; ++i) {
         char current = k[i];
         std::size_t power = k.length()- i - 1;
-        sum += current * pow(10, power)
+        sum += current * pow(10, power);
     }
+
+    return sum;
 }
+
 
 template <class t1, class t2>
 void hashMap<t1, t2>::resize(size_t amount)
@@ -77,9 +80,8 @@ void hashMap<t1, t2>::resize(size_t amount)
     capacity *= amount;
 
     
-    node** temp1 = new node*[newCapacity];
-    node** temp2 = new node*[newCapacity];
-
+    node** temp1 = new node*[capacity];
+    node** temp2 = new node*[capacity];
 
 }
 
@@ -145,8 +147,61 @@ bool hashMap<t1, t2>::find(t1 k)
 template <class t1, class t2>
 void hashMap<t1, t2>::insert(t1 k, t2 v)
 {
-    std::size_t loadFactor = (items1 + items2) /
+    
+    // load factor is number of currently stored / total size of hash table
+    // since there's two tables, add up items and 2*capacity of tables
+    double loadFactor = (items1 + items2) / (2.0 * capacity);
+
+    // if load factor reaches 20%, resize tables x2
+    if (loadFactor >= .20) {
+        resize(2);
+    }
+    
+    // insert node into hash map
+    
+    // create new node with k and v
+    node* insert = new node(k, v);
+    
+    for (std::size_t i = 0 ; i < capacity ; ++i) {
+        // location in table 1
+        std::size_t location1 = hash1(k);
+        // if location is not taken
+        if (table1[location1] == nullptr) {
+            // assign at location
+            table1[location1] = insert;
+            // increment count of items in t1
+            ++items1;
+
+            return;
+        }
+
+        // if location was taken
+        
+        // keep info on node being kicked
+        node* temp = table1[location1];
+        // replace kicked node with inserted node
+        table1[location1] = insert;
+        // prepare to insert kicked node into t2
+        insert = temp;
+
+        // location in table 2
+        std::size_t location2 = hash2(insert->key);
+        if (table2[location2] == nullptr) {
+            table2[location2] = insert;
+            ++items2;
+            return;
+        }
+
+        // if location is taken in t2
+        
+    }
+
+    
 }
+
+    
+    
+    
 
 template <class t1, class t2>
 void hashMap<t1, t2>::update(t1 k, t2 v)
